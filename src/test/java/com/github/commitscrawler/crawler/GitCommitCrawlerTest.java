e221993cfbde3bca53f094af8f92ade6df17316c
menuHwang <hmin0206@naver.com>
2022-10-27 19:48:21 +0900

    불필요한 어노테이션 삭제

--- src/test/java/com/github/commitscrawler/crawler/GitCommitCrawlerTest.java
+++ src/test/java/com/github/commitscrawler/crawler/GitCommitCrawlerTest.java
@@ -3,18 +3,11 @@ package com.github.commitscrawler.crawler;
 import com.github.commitscrawler.domain.commit.CommitPayload;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@ExtendWith(SpringExtension.class)
-@TestConfiguration("classpath:application-local.yml")
 @SpringBootTest
 class GitCommitCrawlerTest {
 
