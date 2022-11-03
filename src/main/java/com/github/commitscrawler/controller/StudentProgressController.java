4f8c6b379cd2f17a24e32c059e2d37f324f97b00
menuHwang <hmin0206@naver.com>
2022-11-03 13:14:15 +0900

    Swagger 설정

--- src/main/java/com/github/commitscrawler/controller/StudentProgressController.java
+++ src/main/java/com/github/commitscrawler/controller/StudentProgressController.java
@@ -3,6 +3,7 @@ package com.github.commitscrawler.controller;
 import com.github.commitscrawler.crawler.GitCommitCrawler;
 import com.github.commitscrawler.domain.commit.CommitPayload;
 import com.github.commitscrawler.lib.enumeration.Subject;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ public class StudentProgressController {
     }
 
     @GetMapping("/list")
-    public ResponseEntity<List<CommitPayload>> getList(@RequestParam("subject") String subjectName) {
+    public ResponseEntity<List<CommitPayload>> getList(@ApiParam(value = "과목 : algorithm / springboot") @RequestParam("subject") String subjectName) {
         // todo crawl한 List를 리턴함
         Subject subject = Subject.valueOf(subjectName.toUpperCase());
         return ResponseEntity.ok().body(gitCommitCrawler.getLatestCommitsAllMember(subject));
