6f6308ad79f1cc4d449dfe1f6207b1e215e4aa98
menuHwang <hmin0206@naver.com>
2022-11-03 09:54:34 +0900

    GitHtmlParser DI 수정

--- src/main/java/com/github/commitscrawler/config/DynamicCommitDetailCrawlerConfig.java
+++ src/main/java/com/github/commitscrawler/config/DynamicCommitDetailCrawlerConfig.java
@@ -2,6 +2,7 @@ package com.github.commitscrawler.config;
 
 import com.github.commitscrawler.crawler.CommitDetailCrawler;
 import com.github.commitscrawler.crawler.GitHtmlParser;
+import com.github.commitscrawler.parser.GitCommitHtmlParser;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
@@ -13,7 +14,7 @@ public class DynamicCommitDetailCrawlerConfig implements BeanPostProcessor {
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if (bean instanceof CommitDetailCrawler && System.getenv().get("GIT_TOKEN") == null){
             System.out.println("GIT_TOKEN의 값이 NULL입니다. GitHtmlParser를 사용합니다.");
-            return new GitHtmlParser(WebClient.builder().baseUrl("https://github.com").build());
+            return new GitHtmlParser(WebClient.builder().baseUrl("https://github.com").build(), new GitCommitHtmlParser());
         }
         return bean;
     }
